3edfd86d7c9636ecc2d37933408b5cc5b3aeeb56
ferzerkerx <ferzerkerx@gmail.com>
2015-07-23 21:27:49 +0000

    - added color indications per category

--- src/templates/js/functions.js
+++ src/templates/js/functions.js
@@ -38,5 +38,34 @@ function drawContributorsGraph(targetTableDataSource, targetCanvas, max) {
         new Chart(ctx).Bar(data);
 }
 
+function drawContributorStats(targetTableDataSource, targetCanvas, colors) {
+
+
+        var percentageTd = $('.' + targetTableDataSource + ' td');
+
+        if (percentageTd.length == 0) {
+            targetCanvas.style.display = 'none';
+            return;
+        }
+
+        var data = [];
+        for (var i = 0; i < percentageTd.length ; i+=3) {
+            var categoryName = percentageTd[i + 1].innerText;
+
+            data.push(
+                {
+                    value: parseInt(percentageTd[i + 2].innerText),
+                    color: colors[categoryName],
+                    label: categoryName
+                }
+            );
+        }
+
+        // Get the context of the canvas element we want to select
+        var ctx = targetCanvas.getContext("2d");
+
+         new Chart(ctx).Pie(data);
+    }
+
 
 
