39701586d4da88a704fbe870ca7d43697b880f91
ferzerkerx <ferzerkerx@gmail.com>
2015-07-23 06:37:02 +0000

    - moved block styles to css file - moved drawing function to js file

---
+++ src/templates/js/functions.js
@@ -0,0 +1,39 @@
+function drawContributorsGraph(targetTableDataSource, targetCanvas, max) {
+        max = typeof max !== 'undefined' ?  max : 20;
+
+        // Get the context of the canvas element we want to select
+        var ctx = targetCanvas.getContext("2d");
+
+        var percentageTd = $('.' + targetTableDataSource + ' td');
+        var labels = []
+                , percentages = [];
+
+        if (percentageTd.length == 0) {
+            targetCanvas.style.display = 'none';
+            return;
+        }
+
+        for (var i = 0; i < percentageTd.length && i < (max * 2) ; i++) {
+            var percentageData = percentageTd[i].innerText;
+            if (i % 2 == 0) {
+                labels.push(percentageData);
+            }
+            else {
+                percentages.push(parseInt(percentageData));
+            }
+        }
+        var data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: targetTableDataSource,
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,0.8)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: percentages
+                }
+            ]
+        };
+        new Chart(ctx).Bar(data);
+    }
