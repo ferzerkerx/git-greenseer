b59c9a89349f99e734bb2cff509892358caf5932
ferzerkerx <ferzerkex@gmail.com>
2015-07-30 21:43:53 -0700

    - created setup file

--- src/main/main.py
+++ src/main/main.py
@@ -5,6 +5,7 @@ import os
 
 print sys.path
 
+from setup import *
 from percentage import PercentageContributorCalculator
 from category_percentage_html_report import render_contribution_percentage_by_committer_using_categories
 from category_percentage_html_report import render_categroy_contribution_percentage_by_committer
@@ -18,79 +19,14 @@ def main():
     if (len(sys.argv) > 1):
         target_dir = sys.argv[1]
 
-    # TODO Fer read this from sdin
-    max = None
-
-    allowed_extenstions = ["java", "ftl", "xml", "xsd", "hbs", "js", "cfm"]
-
-    aliases = {
-        'adt': 'encore',
-        'adtcore': 'encore',
-        'adtcommon': 'encore',
-        'adm': 'admin',
-        'amadeus': 'gds',
-        'sabre': 'gds',
-        'compress': 'asset packaging',
-        'asset_packaging': 'asset packaging',
-        'webcf': 'ots',
-        'implementationresolver': 'stub framework'
-
-    }
-
-    # TODO Fer read this from sdin
-    categories = [
-        'search',
-        'room',
-        'activity',
-        'air',
-        'discount',
-        'insurance',
-        'car',
-        'merchandise',
-        'loyalty',
-        'api',
-        'connector',
-        'multi',
-        'internal',
-        'feedprocessor',
-        'enrichment',
-        'payment',
-        'points',
-        'model',
-        'remoteservice',
-        'repository',
-        'tasks',
-        'report',
-        'encore',
-        'ezmodify',
-        'automation',
-        'stub',
-        'service',
-        'shopping',
-        'mobile',
-        'handlebars',
-        'interceptors',
-        'freemarker',
-        'adm',
-        'marketing',
-        'gds',
-        'validation',
-        'tracking',
-        'asset packaging',
-        'ots',
-        'stub framework',
-        # 'ferzerkerx',
-        # 'web'
-    ]
-
-    sorted_categories = sorted(categories)
+    sorted_categories = sorted(group_report_using_categories)
     percentage_contributor_calculator = PercentageContributorCalculator(target_dir)
-    #TODO Add reporting type
 
-    should_print_by_category = len(categories) > 0
+    should_print_by_category = len(group_report_using_categories) > 0
     if should_print_by_category:
-        stats_by_category = percentage_contributor_calculator.calculate_contribution_percentage_by_committer_using_categories(
-            aliases, sorted_categories, max, allowed_extenstions)
+        stats_by_category = \
+            percentage_contributor_calculator.calculate_contribution_percentage_by_committer_using_categories(
+            category_aliases, sorted_categories, max_number_of_files, allowed_extenstions)
         print 'Generating Contribution Percentage By Category Report'
         html = render_contribution_percentage_by_committer_using_categories(stats_by_category)
         write_file(current_path, html, 'index.html')
---
+++ src/main/setup.py
@@ -0,0 +1,15 @@
+__author__ = 'ferzerkerx'
+
+max_number_of_files = None
+
+allowed_extenstions = ["txt"]
+
+category_aliases = {
+    'categoryx': 'category1' #categoryx will be consiered category1
+}
+
+group_report_using_categories = [
+    'category1',
+    'category2',
+]
+
